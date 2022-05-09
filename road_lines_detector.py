ef80d11a015d4cb4083fb35f2eafcaab6697e0ac
ppiotrlach <lachpio999@gmail.com>
2022-05-09 19:49:22 +0200

    improved

--- road_lines_detector.py
+++ road_lines_detector.py
@@ -5,14 +5,20 @@ import matplotlib.pyplot as plt
 LINES_COLOR_RGB = (255, 0, 0)
 LINE_THICKNESS = 10
 
-# canny contrast
+TEST_VIDEO = "test_LA.mp4"
+
+# Canny contrast
 CANNY_LOW_THRESHOLD = 50
 CANNY_HIGH_THRESHOLD = 150
 
-# region of interest triangle
-TRIANGLE_VERTEX_X_DOWN_LEFT = 180
-TRIANGLE_VERTEX_X_DOWN_RIGHT = 1660
-TRIANGLE_VERTEX_UPPER  = (1000, 550)
+# Region of interest triangle testLA.mp4 (1280x720)
+TRIANGLE_VERTEX_X_DOWN_LEFT = 300
+TRIANGLE_VERTEX_X_DOWN_RIGHT = 1200
+TRIANGLE_VERTEX_UPPER = (630, 370)
+
+# Slopes
+MIN_POSITIVE_SLOPE = 0.8
+MAX_NEGATIVE_SLOPE = -0.8
 
 # HoughLinesP precision
 PIXELS_PRECISION = 2
@@ -21,38 +27,40 @@ POINTS_THRESHOLD = 100
 MIN_LINE_LENGTH = 40
 MAX_LINE_GAP = 5
 
-LINE_START_RATIO = 2 / 5
+LINE_START_RATIO = 3 / 5
 
 def make_coordinates(image, line_params):
     slope, intercept = line_params
     y1 = image.shape[0]
     y2 = int(y1 * LINE_START_RATIO)
-    x1 = int((y1 - intercept)/slope)
-    x2 = int((y2 - intercept)/slope)
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
     return np.array([x1, y1, x2, y2])
 
 def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
-    if lines is None:
-        return np.array([np.array([0, 0, 0, 0]), np.array([0, 0, 0, 0])])
     for line in lines:
         x1, y1, x2, y2 = line.reshape(4)
         parameters = np.polyfit((x1, x2), (y1, y2), 1)
         slope = parameters[0]
         intercept = parameters[1]
-        if slope < 0:
+        if slope < MAX_NEGATIVE_SLOPE:
             left_fit.append((slope, intercept))
-        else:
+        elif slope > MIN_POSITIVE_SLOPE:
             right_fit.append((slope, intercept))
-    if len(left_fit) and len(right_fit):
-        left_fit_average = np.average(left_fit, axis=0)
-        right_fit_average = np.average(right_fit, axis=0)
-        left_line = make_coordinates(image, left_fit_average)
-        right_line = make_coordinates(image, right_fit_average)
-        return np.array([left_line, right_line])
-    return np.array([np.array([0, 0, 0, 0]), np.array([0, 0, 0, 0])])
 
+    result = []
+
+    if len(left_fit):
+        left_fit_average = np.average(left_fit, axis=0)
+        left_line = make_coordinates(image, left_fit_average)
+        result.append(left_line)
+    if len(right_fit):
+        right_fit_average = np.average(right_fit, axis=0)
+        right_line = make_coordinates(image, right_fit_average)
+        result.append(right_line)
+    return np.array(result)
 
 def canny(image):
     # gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -79,40 +87,37 @@ def display_lines(image, lines):
         for line in lines:
             x1, y1, x2, y2 = line.reshape(4)
             cv2.line(line_image, (x1, y1), (x2, y2), LINES_COLOR_RGB, LINE_THICKNESS)
+        
     return line_image
 
-# image = cv2.imread('road2.png')
-# lane_image = np.copy(image)
-# canny_image = canny(lane_image)
+def main():
+    cap = cv2.VideoCapture(TEST_VIDEO)
+    while(cap.isOpened()):
+        ret, frame =  cap.read()
+        if ret:
+            canny_image = canny(frame)
 
-# cropped_image = region_of_interest(canny_image)
+            cropped_image = region_of_interest(canny_image)
 
-# lines = cv2.HoughLinesP(cropped_image, PIXELS_PRECISION, DEGREE_PRECISION, POINTS_THRESHOLD, np.array([]), minLineLength = MIN_LINE_LENGTH, maxLineGap = MAX_LINE_GAP)
-# averaged_lines = average_slope_intercept(lane_image, lines)
-# line_image = display_lines(lane_image, lines)
+            lines = cv2.HoughLinesP(cropped_image, PIXELS_PRECISION, DEGREE_PRECISION, POINTS_THRESHOLD, np.array([]),
+                minLineLength = MIN_LINE_LENGTH, maxLineGap = MAX_LINE_GAP)
+            if lines is not None:
+                averaged_lines = average_slope_intercept(frame, lines)
+                line_image = display_lines(frame, averaged_lines)
+                # line_image = display_lines(frame, lines)
 
-# combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-# plt.imshow(combo_image)
-# plt.show()
-# cv2.imshow("image", cropped_image)
-# cv2.waitKey(0)
+                combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+                cv2.imshow("Frame", combo_image) # main
+                # cv2.imshow("Frame", cropped_image)
+            else:
+                cv2.imshow("Frame", frame) # main
+                # cv2.imshow("Frame", cropped_image)
+            if cv2.waitKey(30) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
-cap = cv2.VideoCapture("test4.mp4")
-while(cap.isOpened()):
-    _, frame =  cap.read()
-    canny_image = canny(frame)
-
-    cropped_image = region_of_interest(canny_image)
-
-    lines = cv2.HoughLinesP(cropped_image, PIXELS_PRECISION, DEGREE_PRECISION, POINTS_THRESHOLD, np.array([]), minLineLength = MIN_LINE_LENGTH, maxLineGap = MAX_LINE_GAP)
-    averaged_lines = average_slope_intercept(frame, lines)
-    line_image = display_lines(frame, averaged_lines)
-
-    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-    # plt.imshow(combo_image)
-    # plt.show()
-    cv2.imshow("image", combo_image)
-    if cv2.waitKey(30) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
